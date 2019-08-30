8ded519749bd2987fd9ec965b702fcca3b470fca
Leighton <lechen@microsoft.com>
2019-08-30 11:58:32 -0700

    logs

--- contrib/opencensus-ext-flask/examples/sample.py
+++ contrib/opencensus-ext-flask/examples/sample.py
@@ -14,7 +14,7 @@ middleware = FlaskMiddleware(application)
 
 @application.route("/")
 def hello():
-    logger.warning("Testing!")
+    logger.warning("Testing Logs!")
     return "<h1 style='color:blue'>Hello There!</h1>"
 
 if __name__ == "__main__":
